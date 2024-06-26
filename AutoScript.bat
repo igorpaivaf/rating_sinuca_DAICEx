0e4cb25a210384d446b0670772ede4ee22f878bd
Natan <135863264+natanaelo-jr@users.noreply.github.com>
2024-06-25 21:42:35 -0300

    troca um comando do autoscript
    o autoscript agora deve conseguir atualizar o repositório do github direto

--- AutoScript.bat
+++ AutoScript.bat
@@ -2,6 +2,5 @@ cd "c:/Users/igorp/OneDrive/Área de Trabalho/VSCode/rating_sinuca"
 python rating_sinuca.py
 python tableconvert.py
 git commit -m "Data"
-git pull origin main --allow-unrelated-histories
-github
+git push
 
