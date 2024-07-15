20dc78483761b437ad7c093f0069b2f96b70caa2
Igor Paiva <igorpaivaf@gmail.com>
2024-07-15 18:44:03 -0300

    Data_upload

--- rating_sinuca.py
+++ rating_sinuca.py
@@ -114,11 +114,7 @@ def exibir_ratings(ratings):
 # Exemplo de resultados: [(jogador1, jogador2, resultado, tipo_jogo), ...]
 # resultado é a diferença de partidas ganhas (ex: 2 para 2-0 em melhor de 3, 1 para 3-2 em melhor de 5)
 resultados_exemplo = [
-    ('Luiz', 'White', 1, 1),
-    ('PT', 'Joao', 1, 1),
-    ('PT', 'MT', 1, 1),
-    ('PT', 'Cristian', 1, 1),
-    ('Souza', 'Daniel', 2, 3)
+    ('Xaropinho', 'PT', 1, 3)
 ]
 
 # Executar as funções
