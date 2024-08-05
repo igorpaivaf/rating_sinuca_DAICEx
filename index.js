0b77de663c79b8adc16cde77e9350fb90a4cd1e0
Natanael dos Santos Júnior <natanael0junior@gmail.com>
2024-08-05 20:37:10 -0300

    Adiciona a posição dos jogadores no ranking

--- index.js
+++ index.js
@@ -12,13 +12,36 @@ function fetchData(){
             return res.json();
         })
         .then((data) => {
-            
-            data.forEach(element => root.insertAdjacentHTML('beforebegin', `<tr class="hover:bg-zinc-800 hover:text-zinc-100"><td class="text-zinc-500 p-2">${element.name}</td><td  class="text-zinc-500">${element.rating}</td></tr>`));        }
+            let position = 1
+            const sortedData = [...data].sort((a, b) => b.rating - a.rating)
+
+            const positionMap = {};
+            sortedData.forEach((element, index) => {
+                // Verifica se a posição já foi atribuída ao mesmo rating
+                if (positionMap[element.rating] === undefined) {
+                    positionMap[element.rating] = index + 1;
+                }
+            });
+
+            const dataWithPosition = data.map(element => ({
+                ...element,
+                position: positionMap[element.rating]
+            }));
+
+            dataWithPosition.sort((a, b) => a.name.localeCompare(b.name));
+
+            dataWithPosition.forEach(element => root.insertAdjacentHTML('beforebegin', 
+                `<tr class="hover:bg-zinc-800 hover:text-zinc-100">
+                    <td class="text-zinc-500 p-2">${element.position}</td>
+                    <td class="text-zinc-500 p-2">${element.name}</td>
+                    <td  class="text-zinc-500">${element.rating}</td>
+                </tr>`));        }
         )
         .catch((error) => 
             console.error("Nao achei"));
 }
 function fetchData2(){
+    let position = 1
     fetch('./data.json')
         .then((res) =>{
             if (!res.ok){
@@ -29,7 +52,13 @@ function fetchData2(){
         })
         .then((data) => {
             data.sort((a, b) => b.rating - a.rating)
-            data.forEach(element => sorted.insertAdjacentHTML('beforebegin', `<tr class="hover:bg-zinc-800 hover:text-zinc-100"><td class="text-zinc-500 p-2">${element.name}</td><td  class="text-zinc-500">${element.rating}</td></tr>`));        }
+            data.forEach(element => sorted.insertAdjacentHTML(
+                'beforebegin', 
+                `<tr class="hover:bg-zinc-800 hover:text-zinc-100">
+                    <td class="text-zinc-500 p-2">${position++}°</td>
+                    <td class="text-zinc-500 p-2">${element.name}</td>
+                    <td  class="text-zinc-500">${element.rating}</td>
+                    </tr>`));        }
         )
         .catch((error) => 
             console.error("Nao achei"));
