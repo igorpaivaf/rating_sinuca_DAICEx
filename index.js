efdcb78dfc5e4418d8cc7bab238c73b0ce043ce9
Natanael dos Santos Júnior <natanael0junior@gmail.com>
2024-08-05 21:06:46 -0300

    adiciona responsividade

--- index.js
+++ index.js
@@ -12,29 +12,11 @@ function fetchData(){
             return res.json();
         })
         .then((data) => {
-            let position = 1
-            const sortedData = [...data].sort((a, b) => b.rating - a.rating)
 
-            const positionMap = {};
-            sortedData.forEach((element, index) => {
-                // Verifica se a posição já foi atribuída ao mesmo rating
-                if (positionMap[element.rating] === undefined) {
-                    positionMap[element.rating] = index + 1;
-                }
-            });
-
-            const dataWithPosition = data.map(element => ({
-                ...element,
-                position: positionMap[element.rating]
-            }));
-
-            dataWithPosition.sort((a, b) => a.name.localeCompare(b.name));
-
-            dataWithPosition.forEach(element => root.insertAdjacentHTML('beforebegin', 
+            data.forEach(element => root.insertAdjacentHTML('beforebegin', 
                 `<tr class="hover:bg-zinc-800 hover:text-zinc-100">
-                    <td class="text-zinc-500 p-2">${element.position}</td>
                     <td class="text-zinc-500 p-2">${element.name}</td>
-                    <td  class="text-zinc-500">${element.rating}</td>
+                    <td class="text-zinc-500">${element.rating}</td>
                 </tr>`));        }
         )
         .catch((error) => 
